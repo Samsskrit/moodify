d55baf78a77ae1789196b3d7494350948bd1ed34
irahulll <rahul1221.iyer@gmail.com>
2023-10-14 14:15:33 -0400

    started working on frontend

---
+++ moodify-webpage/src/components/UploadForm.vue
@@ -0,0 +1,27 @@
+<template>
+    <form @submit="onSubmit()">
+        <input id="uploadImage" type="file" @change="displayImage()" required/>
+        <img id="preview" src="#" alt="uploaded image">
+        <input type="submit" value="Upload Image"/>
+    </form>
+</template>
+
+<script>
+    export default {
+        name: 'Form',
+        methods: {
+            displayImage() {
+                console.log(uploadImage.files);
+                const [file] = uploadImage.files;
+                if (file) {
+                    preview.src = URL.createObjectURL(file);
+                }
+            },
+            onSubmit(e) {
+                console.log("hiii");
+                e.preventDefault();
+
+            }
+        }
+    }
+</script>
