e36b48206b00177ae2c2f6f5d44488ebdfdd7826
irahulll <rahul1221.iyer@gmail.com>
2023-10-15 02:02:50 -0400

    updated webpage lol

--- tailwind-vue/src/components/UploadForm.vue
+++ tailwind-vue/src/components/UploadForm.vue
@@ -1,8 +1,20 @@
 <template>
     <form @submit="onSubmit()">
-        <input id="uploadImage" type="file" ref="file" @change="handleFileUpload($event)" required/>
-        <img id="preview" src="#" alt="uploaded image">
-        <input type="submit" value="Upload Image"/>
+      <div class = "center">
+        <button type="file" class ="flex items-center justify-start">
+            <img src="images/add.png" class ="h-8 w-8 mr-3" style="filter: brightness(0) invert(1);"/>
+              <body text="white" style="font-family:cursive;">
+                <p style="font-size:large">
+                  <label for="uploadImage">
+                    Create Mood Mix
+                  </label>
+                </p>
+              </body>
+              <input id="uploadImage" type="file" ref="file" @change="handleFileUpload($event)" required/>
+        </button> <br>
+        <input id = "submitBtn" type="submit" value="I'm happy with this image!"/>
+      </div>
+      <img id="preview" src="#" alt="uploaded image">
     </form>
 </template>
 
@@ -37,7 +49,7 @@
             onSubmit(e) {
                 const newPlaylist = {
                     imageLink: base64String,
-                    playlist: 'testing bitch playlists new',
+                    playlist: 'testing playlists new',
                 }
                 this.$emit('upload', newPlaylist)
 
@@ -46,3 +58,47 @@
         }
     }
 </script>
+<style scoped>
+label {
+   cursor: pointer;
+   color: white;
+   top: 50%;
+   font-size: 50px;
+   text-decoration:underline;
+}
+
+#uploadImage {
+   opacity: 0;
+   position: absolute;
+   z-index: -1;
+}
+
+#submitBtn {
+    cursor: pointer;
+    font-family: cursive;
+    color: white;
+    border: 2px solid whitesmoke;
+    background-color: grey;
+    position: absolute;
+    width: 450px;
+}
+
+#preview {
+    color: grey;
+    object-fit: cover;
+    display: block;
+    max-width:400px;
+    max-height:400px;
+    width: auto;
+    height: auto;
+    position: absolute;
+    top: 40%;
+    right: 35%;
+}
+
+.center {
+    position: absolute;
+    top: 20%;
+    left: 32%;
+}
+</style>
