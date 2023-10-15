bc22d787f0f4b0ea4d74e5448010b70b15fa2983
Michael Chian <michael.chian9@gmail.com>
2023-10-14 21:59:23 -0400

    front end working from upload to api

---
+++ moodify-webpage/src/components/Playlist.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="playlist">
+        <h3>{{ playlist.id }}</h3>
+        <h3>{{ playlist.playlist }}</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Playlist',
+        props: {
+            playlist: Object
+        }
+    }
+</script>
